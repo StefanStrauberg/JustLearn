dc9468718a13e23b762d1d18e7f0ad4a73c9d651
Strauberg <StefanStrauberg@gmail.com>
2025-04-12 21:09:15 +0300

    Added
    - Working with Basic Console IO
    - Formatin Numerical Data

---
+++ Chapter_3/07_BasicConsoleIO/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write(new string('*', 5));
+Console.Write(" Basic Console IO ");
+Console.WriteLine(new string('*', 5));
+
+GetUserData(out string name, out int age);
+SetColorConsole();
+Print($"Hello {name}! You're {age} years old!");
+Console.ResetColor();
+Console.WriteLine();
+
+static void SetColorConsole()
+{
+  Console.BackgroundColor = ConsoleColor.Yellow;
+  Console.ForegroundColor = ConsoleColor.Blue;
+}
+
+static void Print(string message)
+  => Console.Write(message);
+
+
+static void GetUserData(out string name, out int age)
+{
+  Console.Write("Please enter your name: ");
+  var input = Console.ReadLine();
+  name = string.IsNullOrWhiteSpace(input) == true ? "unknown" : input;
+  Console.Write("Please enter your age: ");
+  if (Int32.TryParse(Console.ReadLine(), out age)) {}
+  age = default;
+}
---
+++ Chapter_3/08_FormatNumericalData/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write(new string('*', 5));
+Console.Write(" Format Numerical Data ");
+Console.WriteLine(new string('*', 5));
+
+FormatNumericalData();
+
+static void FormatNumericalData()
+{
+  Console.WriteLine("The value 99999 in various formats:");
+  Console.WriteLine($"c format: {99999:c}");
+  Console.WriteLine($"d9 format: {99999:d9}");
+  Console.WriteLine($"f3 format: {99999:f3}");
+  Console.WriteLine($"n format: {99999:n}");
+
+  Console.WriteLine($"E format: {99999:E}");
+  Console.WriteLine($"e format: {99999:e}");
+  Console.WriteLine($"X format: {99999:X}");
+  Console.WriteLine($"x format: {99999:x}");
+}
