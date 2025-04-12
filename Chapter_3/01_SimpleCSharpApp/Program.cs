1b5a29377ea22a64a8551f7c1bfb8a50c5e8c143
Strauberg <StefanStrauberg@gmail.com>
2025-04-12 12:32:35 +0300

    Created Simple C Sharp - Created classic C Sharp App - Created the same one with using top-level operators

---
+++ Chapter_3/01_SimpleCSharpApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_SimpleCSharpApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("*** My First C# App ***");
+      Console.WriteLine("Hello World!");
+      Console.WriteLine();
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ Chapter_3/02_TopLevelOperatorApp/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("*** My First C# App ***");
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+Console.ReadLine();
